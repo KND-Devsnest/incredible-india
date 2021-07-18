9493209d386a8d9569887efecf326a110fced82f
seebham <shubhamisdev@gmail.com>
2021-07-18 22:01:12 +0530

    hero edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import styles from "../styles/Hero.module.css";
 
 const Hero = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <video
+        poster="/content/dam/incredibleindia/video/home/monsoon-banner.jpg/jcr:content/renditions/cq5dam.web.1800.737.jpeg"
+        preload="auto"
+        autoPlay="autoplay"
+        muted="muted"
+        loop="loop"
+      >
+        <source src="https://aguyran.me/media/tourIndia1080Full.mp4" type="video/mp4" />
+      </video>
+      <h2 className={styles.heroCaption}>Incredible India</h2>
+    </div>
+  );
 };
 
 export default Hero;
